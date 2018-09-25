0edce231787332ab1287dc4d74ef36a4fa90bf65
Tara Vancil <tbvanc@gmail.com>
2018-09-25 13:17:04 -0500

    Add error for invalid file type

--- js/const.js
+++ js/const.js
@@ -1,6 +1,7 @@
 export const ERRORS = {
   invalidProtocol: 'Please enter a dat:// or https:// URL',
   invalidImageUrl: 'Please enter a valid image URL (.png, .jpg, .jpeg)',
+  invalidFileType: 'Invalid file type',
   timedOut: 'File not found. Request timed out',
   notFound: 'File not found'
 }
